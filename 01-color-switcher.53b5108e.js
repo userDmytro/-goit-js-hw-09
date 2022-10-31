0885928db682c6a998778a99736f4316d775e431
userDmytro <104132845+userDmytro@users.noreply.github.com>
2022-10-31 03:06:02 +0000

    Deploying to gh-pages from @ userDmytro/-goit-js-hw-09@670f59e0186c22f93aefe399a8910d12ea8b79aa 🚀

---
+++ 01-color-switcher.53b5108e.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]");let d=null;t.disabled=!0,e.addEventListener("click",(function(){d=setInterval((()=>{document.body.style.background=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3),e.disabled=!0,t.disabled=!1})),t.addEventListener("click",(function(){clearInterval(d),e.disabled=!1,t.disabled=!0}));
+//# sourceMappingURL=01-color-switcher.53b5108e.js.map
---
+++ 01-color-switcher.d48ebde6.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),d=null;e.disabled=!0,t.addEventListener("click",(function(){d=setInterval((function(){document.body.style.background="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3),t.disabled=!0,e.disabled=!1})),e.addEventListener("click",(function(){clearInterval(d),t.disabled=!1,e.disabled=!0}))}();
+//# sourceMappingURL=01-color-switcher.d48ebde6.js.map
